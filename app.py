32a00b4df66e07bd163676ed5538566c6890074e
ABDULRAHMAN <dhmez331@gmail.com>
2026-02-18 19:26:02 +0300

    Update GoogleGenerativeAIEmbeddings model to text-embedding-004 in initialize_rag function

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def initialize_rag():
         splits = text_splitter.split_documents(docs)
         
         # استخدام Embeddings جوجل السحابية لتوفير الذاكرة (RAM)
-        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
         
         # إنشاء مخزن المتجهات (Vector Store)
         vector_store = FAISS.from_documents(splits, embeddings)
